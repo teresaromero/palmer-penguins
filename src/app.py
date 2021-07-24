0ebb462385d2603f2112f603700a44e92acb77dc
Tere <hello@teresaromero.dev>
2021-07-24 22:36:26 +0200

    [resolves #4] - Integrate DB with app (#21)

--- src/app.py
+++ src/app.py
@@ -1,3 +1,7 @@
 from flask import Flask
+from config import MONGO_DBNAME, MONGO_URI
 
 app = Flask("palmer-penguins-api")
+
+app.config['MONGO_DBNAME'] = MONGO_DBNAME
+app.config['MONGO_URI'] = MONGO_URI
--- src/config.py
+++ src/config.py
@@ -5,3 +5,5 @@ dotenv.load_dotenv()
 
 PORT = os.getenv("PORT")
 FLASK_ENV = os.getenv("FLASK_ENV")
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DBNAME = os.getenv("MONGO_DBNAME")
