bafa337ad3ff359c920ee89d61517312f89d35a3
Teresa <hello@teresaromero.dev>
2021-07-28 00:03:33 +0200

    🪄refactor: Added error handling on MongoDB connection

--- src/libs/mongo.py
+++ src/libs/mongo.py
@@ -1,7 +1,14 @@
 from flask_pymongo import PyMongo
 from app import app
+import os
 
-mongo = PyMongo(app)
+mongo = PyMongo(app, serverSelectionTimeoutMS=1000)
+try:
+    mongo.db.command('ping')
+    app.logger.info("MongoDB connexion successful")
+except Exception as e:
+    app.logger.error(str(e))
+    os._exit(1)
 
 
 def collection(name): return mongo.db[name]
