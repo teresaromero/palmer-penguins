638072689e265b00b57a66b3ad46d76f36a9f533
Teresa <hello@teresaromero.dev>
2021-07-30 07:18:46 +0200

    ✨ feat: streamlit: Added service to docker-compose

--- streamlit/config.py
+++ streamlit/config.py
@@ -3,5 +3,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_URL = os.getenv("API_URL") if os.getenv(
-    "API_URL") else "http://localhost:5000"
+API_URL = f"http://{os.getenv('API_HOST')}:{os.getenv('API_PORT')}"
--- streamlit/data/requests.py
+++ streamlit/data/requests.py
@@ -5,13 +5,9 @@ import streamlit as st
 
 @st.cache
 def get_all_penguins():
-    data = []
-    path = "/penguins"
-
-    while path:
-        response = requests.get(f"{API_URL}{path}")
-        if response.status_code == 200:
-            res = response.json()
-            path = res["_page"]["next"]
-            data += res["data"]
-    return data
+    response = requests.get(f"{API_URL}/penguins?page=0")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {}
+    
