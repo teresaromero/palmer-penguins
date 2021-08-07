bd4bb787b76fffdfe9c4812853904b801c6c29bd
Teresa <hello@teresaromero.dev>
2021-08-07 13:26:49 +0200

    ✨ feat: streamlit: first data viz 🍾

--- streamlit/components/individuals.py
+++ streamlit/components/individuals.py
@@ -1,17 +1,48 @@
-from json import dumps
-from pandas.io import json
 import streamlit as st
 import pandas as pd
 from api.individuals import fetch_all
+import logging
+
+state = st.session_state
+
+hist_fields = ["culmen_length", "culmen_depth",
+               "flipper_length", "body_mass"]
 
 
 def show():
-    st.header("📊 Individuals description")
+    st.header("📊 General Data")
     st.subheader("This is a subheader")
+    df = pd.DataFrame()
+
     data = fetch_all()
     if data:
         df = pd.DataFrame(data)
-        st.dataframe(df)
-        st.table(df.describe())
     else:
+        logging.error("Error retrieving data from API")
         st.error("Oops! Seems we are having trouble fetching the info")
+
+    st.subheader("Individuals Sex Distribution")
+    sex_dist = df["sex"].value_counts().to_dict()
+    values = [{"category": v, "value": sex_dist[v]} for v in sex_dist.keys()]
+    st.vega_lite_chart(values, {
+        "mark": {"type": "arc", "innerRadius": 50},
+        "encoding": {
+            "theta": {"field": "value", "type": "quantitative"},
+            "color": {"field": "category", "type": "nominal"}
+        },
+        "view": {"stroke": None}
+
+    }, use_container_width=True)
+
+    st.subheader("Data Histogram")
+    for c in hist_fields:
+        st.vega_lite_chart(df, {
+            "mark": "bar",
+            "encoding": {
+                "x": {
+                    "bin": True,
+                    "field": c
+                },
+                "y": {"aggregate": "count"}
+            }
+        }, use_container_width=True)
