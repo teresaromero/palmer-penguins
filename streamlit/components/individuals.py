4d1974be931d73e206f489f13769e08344668f79
Teresa <hello@teresaromero.dev>
2021-08-07 20:13:53 +0200

    🪄refactor: streamlit: refactor and added components

--- streamlit/components/individuals.py
+++ streamlit/components/individuals.py
@@ -1,28 +1,29 @@
+from pandas.core.frame import DataFrame
 import streamlit as st
-import pandas as pd
-from api.individuals import fetch_all
-import logging
-
-state = st.session_state
 
 hist_fields = ["culmen_length", "culmen_depth",
                "flipper_length", "body_mass"]
 
 
-def show():
-    st.header("📊 General Data")
-    st.subheader("This is a subheader")
-    df = pd.DataFrame()
+def histogram(df_individuals: DataFrame, fields: list[str]):
+    st.subheader("All Individuals Histograms")
+    for c in fields:
+        st.text(c.replace("_", " ").title())
+        st.vega_lite_chart(df_individuals, {
+            "mark": "bar",
+            "encoding": {
+                "x": {
+                    "bin": True,
+                    "field": c
+                },
+                "y": {"aggregate": "count"}
+            }
+        }, use_container_width=True)
 
-    data = fetch_all()
-    if data:
-        df = pd.DataFrame(data)
-    else:
-        logging.error("Error retrieving data from API")
-        st.error("Oops! Seems we are having trouble fetching the info")
 
+def sex_dist(df_individuals: DataFrame):
     st.subheader("Individuals Sex Distribution")
-    sex_dist = df["sex"].value_counts().to_dict()
+    sex_dist = df_individuals["sex"].value_counts().to_dict()
     values = [{"category": v, "value": sex_dist[v]} for v in sex_dist.keys()]
     st.vega_lite_chart(values, {
         "mark": {"type": "arc", "innerRadius": 50},
@@ -33,16 +34,3 @@ def show():
         "view": {"stroke": None}
 
     }, use_container_width=True)
-
-    st.subheader("Data Histogram")
-    for c in hist_fields:
-        st.vega_lite_chart(df, {
-            "mark": "bar",
-            "encoding": {
-                "x": {
-                    "bin": True,
-                    "field": c
-                },
-                "y": {"aggregate": "count"}
-            }
-        }, use_container_width=True)
---
+++ streamlit/components/species.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from pandas.core.frame import DataFrame
+from streamlit.elements.arrow import Data
+
+
+def dist_prop_by_species(df: DataFrame, prop: str):
+    title = prop.replace("_", " ").title()
+    st.subheader(f"Distribution of {title} By Species")
+    vega_area_chart(df, prop, "name_species", title)
+
+
+def vega_area_chart(df: Data, prop: str, group: str, title: str):
+    st.vega_lite_chart(df, {
+        "mark": "area",
+        "transform": [
+            {
+                "density": prop,
+                "groupby": [group],
+                "extent": [df[prop].min(), df[prop].max()],
+            }
+        ],
+        "encoding": {
+            "x": {"field": "value", "type": "quantitative", "title": title},
+            "y": {"field": "density", "type": "quantitative"},
+            "color": {"field": group, "type": "nominal"}
+        }
+
+    }, use_container_width=True)
