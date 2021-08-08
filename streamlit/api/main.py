cc2aa1421d8770926576ade1e8f2e0e956086e22
Teresa <hello@teresaromero.dev>
2021-08-08 13:40:59 +0200

    ✨ feat: streamlit: new api call to update individual

--- streamlit/api/main.py
+++ streamlit/api/main.py
@@ -10,3 +10,17 @@ def request_api(source: str):
         return response.json()
     else:
         return None
+
+
+def update_individual(id: str, payload: dict):
+    response = requests.patch(
+        f"{API_URL}/individuals/{id}", json=payload)
+    if response.status_code == 200:
+        st.balloons()
+        return response.json()
+    elif response.status_code == 404 or response.status_code == 400:
+        st.warning(response.json()["error"])
+        return None
+    else:
+        st.error(response.json())
+        return None
