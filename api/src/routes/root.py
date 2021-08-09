a26fdfa1a466aafab2f1973d1972d6333d0250fa
Teresa <hello@teresaromero.dev>
2021-08-09 08:38:47 +0200

    🪄refactor: api: structure project change

--- api/src/routes/root.py
+++ api/src/routes/root.py
@@ -1,7 +1,7 @@
-from libs.validation import validate_route
+from decorators.validation import validate_route
 from flask import request
 from libs.response import response
-from libs.errors import handle_error
+from decorators.errors import handle_error
 from controllers.root import get_all, find_by_id_and_update
 from app import app
 
