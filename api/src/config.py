e620a7ac6950173e66e9b98390da9e336f29fc7c
Teresa <hello@teresaromero.dev>
2021-08-13 06:04:31 +0200

    ⚙ ci️: refactor dc

--- api/src/config.py
+++ api/src/config.py
@@ -8,11 +8,4 @@ FLASK_DEBUG = os.getenv("FLASK_DEBUG", 1)
 FLASK_HOST = os.getenv("FLASK_HOST", "0.0.0.0")
 FLASK_APP = os.getenv("FLASK_APP", "palmer-app")
 
-
-MONGO_USERNAME = os.getenv("MONGO_USERNAME")
-MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
-MONGO_HOST = os.getenv("MONGO_HOST")
-MONGO_PORT = os.getenv("MONGO_PORT", 27017)
-MONGO_DBNAME = os.getenv("MONGO_DBNAME")
-
-MONGO_URI = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DBNAME}"
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/palmer-penguins")
--- streamlit/config.py
+++ streamlit/config.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_URL = f"http://{os.getenv('API_HOST')}:{os.getenv('API_PORT')}"
+API_URL = os.getenv('API_URL', "http://localhost:3000")
